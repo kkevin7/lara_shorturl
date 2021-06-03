8388c7ebb44c90d633719617b8acd21246b93176
Kevin Martinez <kkevinmartinez7@gmail.com>
2021-06-02 20:47:43 -0600

    url view and javascript with some methods

---
+++ resources/js/shorter.js
@@ -0,0 +1,32 @@
+window.shorter = {
+    short_url: () => {
+        let long_url = document.getElementById('long_url').value;
+        if(shorter.validate_url()){
+
+            axios.post('/url',{
+                long_url : long_url
+            }).then((response) => {
+                document.getElementById('long_url').value = '';
+                document.getElementById('short_url_container').style.display = 'block';
+                document.getElementById('short_url').value = response.data.short_url;
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+    },
+
+    copy_url: () => {
+        let short_url = document.getElementById('short_url');
+        short_url.select();
+        short_url.setSelectionRange(0,99999);
+        document.execCommand('copy');
+    },
+
+    validate_url: (url) => {
+        let status = true;
+        if(url.trim() === ""){
+            status = false;
+            alert('Debe colocar una URL');
+        }
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+
+                    <div class="flex">
+                        <input id="logn_url" type="text" name="" placeholder="Pega la URL aqui"
+                            class="w-full px-4 py-3 rounded">
+                        <button
+                            class="ml-4 w-auto px-6 py-3 font-semibold bg-gray-900 text-white rounded"
+                            onclick="shorter.short_url()"
+                            ><span>Acortar</span></button>
+                    </div>
+
+                    <div id="short_url_container" class="flex py-5" style="display: none;">
+                        <input id="shor_url" type="text" placeholder="" class="w-full px-4 py-3 rounded"
+                        onclick="shorter.copy_url()"
+                        >
+                    </div>
+
                 </div>
             </div>
         </div>
